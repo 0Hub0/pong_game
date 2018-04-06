88190300717ffd5113977a940208144f4252431d
Gaétan Guru <gaetan.guru@gmail.com>
2018-04-06 12:58:20 +0200

    Popup menu added

--- app/src/main/java/com/example/a13022/pong_game/MainActivity.java
+++ app/src/main/java/com/example/a13022/pong_game/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.a13022.pong_game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        popUpMenu();
+
     }
+    // Creates the popup menu in order to select whether we want to play with another person or solo.
+    public void popUpMenu(){
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, button);
+                //TODO : revoir signification des "Inflater"
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, "You clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
+    }
+
 }
