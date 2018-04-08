76ae7d4bafedce5b581b9b8cdb389c84c5a5cce6
Gaétan Guru <gaetan.guru@gmail.com>
2018-04-09 00:24:31 +0200

    Popup menu added

--- app/src/main/java/com/example/a13022/pong_game/MainActivity.java
+++ app/src/main/java/com/example/a13022/pong_game/MainActivity.java
@@ -5,31 +5,53 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public String opponentName = "";
+    public String username="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         popUpMenu();
-
+        settingsButton();
+        goButton();
+        Toast.makeText(MainActivity.this, getUser2(), Toast.LENGTH_SHORT).show();
     }
     // Creates the popup menu in order to select whether we want to play with another person or solo.
     public void popUpMenu(){
         final Button button = findViewById(R.id.button_id);
+        final EditText opponent_name = findViewById(R.id.username_opponent);
+        final EditText username = findViewById(R.id.username);
+        final Button button_go = findViewById(R.id.button_selection);
+        opponent_name.setVisibility(View.INVISIBLE);
+        button_go.setVisibility(View.INVISIBLE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PopupMenu popup = new PopupMenu(MainActivity.this, button);
-                //TODO : revoir signification des "Inflater"
                 popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
                     public boolean onMenuItemClick(MenuItem item) {
-                        Toast.makeText(MainActivity.this, "You clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "You clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        if(item.getTitle().equals("Multiplayer Player")){
+                            opponent_name.setVisibility(View.VISIBLE);
+                            button_go.setVisibility(View.VISIBLE);
+                        }
+                        else{
+                            opponent_name.setVisibility(View.INVISIBLE);
+                            button_go.setVisibility(View.INVISIBLE);
+                            setUsername(username.getText().toString());
+                            Toast.makeText(MainActivity.this, "You clicked : " + getUsername(), Toast.LENGTH_SHORT).show();
+                            //TODO : If the player plays against the computer, the next page should be the game
+                        }
                         return true;
                     }
                 });
@@ -37,5 +59,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void goButton(){
+       final Button button = findViewById(R.id.button_selection);
+       final EditText username = findViewById(R.id.username);
+       final EditText opponent_name = findViewById(R.id.username_opponent);
+       button.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+                //TODO : when button "Go" is pressed the next page should either the game or a loading page to connect both the players.
+                setUsername(username.getText().toString());
+                setUser2(opponent_name.getText().toString());
+                Toast.makeText(MainActivity.this, "You clicked : " + getUsername() + " , " + getUser2(), Toast.LENGTH_SHORT).show();
+           }
+       });
+    }
 
+    public void settingsButton(){
+        final ImageButton button = findViewById(R.id.button_settings);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //TODO : direction towards the page settings has to be put here
+                Toast.makeText(MainActivity.this, "You clicked the robot DAMN YOU", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void setUser2(String opponentName){
+        this.opponentName = opponentName;
+    }
+    public String getUser2(){
+        return this.opponentName;
+    }
+    public void setUsername(String username){
+        if(username.isEmpty() || username.equals("")) {
+            username = "Default User";
+        }
+        this.username = username;
+    }
+    public String getUsername(){
+        return this.username;
+    }
 }
