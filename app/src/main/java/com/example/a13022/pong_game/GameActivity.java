d3e794d1ff239620b247c5914ac7c2260c208204
Gaétan Guru <gaetan.guru@gmail.com>
2018-04-13 19:50:22 +0200

    gameBranch, added a few classes. Still a lot of changes before being totally functional.

--- app/src/main/java/com/example/a13022/pong_game/GameActivity.java
+++ app/src/main/java/com/example/a13022/pong_game/GameActivity.java
@@ -1,13 +1,43 @@
 package com.example.a13022.pong_game;
 
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-
+    PongView pongView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        // Get a Display object to access screen details
+        Display display = getWindowManager().getDefaultDisplay();
+        // Load the resolution into a Point object
+        Point size = new Point();
+        display.getSize(size);
+        // Initialize pongView and set it as the view
+        pongView = new PongView(this,size.x,size.y);
+        setContentView(pongView);
+    }
+    // This method executes when the player starts the game
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Tell the pongView resume method to execute
+        pongView.resume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the pongView pause method to execute
+        pongView.pause();
     }
 }
+
