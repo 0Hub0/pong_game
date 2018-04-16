b740e77b9543722f7c3fb5eee3052007eb1a8069
Gaétan Guru <gaetan.guru@gmail.com>
2018-04-17 01:02:30 +0200

    Added class PongView

---
+++ app/src/main/java/com/example/a13022/pong_game/PongView.java
@@ -0,0 +1,13 @@
+package com.example.a13022.pong_game;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by Gaetan on 16-04-18.
+ */
+
+public class PongView extends SurfaceView implements Runnable{
+    public void run(){
+
+    }
+}
